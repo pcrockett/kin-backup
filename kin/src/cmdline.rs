bd10b3d96034c2b675af51cf9826cd85c2944de5
Philip Crockett <contact@philcrockett.com>
2018-12-15 19:02:33 +0100

    Testing CliArgs enum

--- kin/src/cmdline.rs
+++ kin/src/cmdline.rs
@@ -27,3 +27,38 @@ pub enum CliArgs {
 pub fn parse() -> CliArgs {
     CliArgs::from_args()
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn init_no_dir() {
+        let args = [ "kin", "init" ].iter();
+
+        let parsed = CliArgs::from_iter(args);
+        match parsed {
+            CliArgs::Init { directory } => assert_eq!(directory, Option::None),
+            _ => panic!("not an init subcommand")
+        };
+    }
+
+    #[test]
+    fn init_with_dir() {
+
+        let args = [ "kin", "init", "foo/bar" ].iter();
+
+        let parsed = CliArgs::from_iter(args);
+        match parsed {
+            CliArgs::Init { directory } => {
+                match directory {
+                    Some(path) => assert_eq!("foo/bar", path.to_str().unwrap()),
+                    None => panic!("no directory specified")
+                }
+            }
+            _ => panic!("not an init subcommand")
+        };
+
+    }
+}
