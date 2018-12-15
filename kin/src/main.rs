aacb2b7711ecdd75dc281662af0414e69c47748f
Philip Crockett <contact@philcrockett.com>
2018-12-15 18:24:54 +0100

    Moved CLI stuff into its own module

--- kin/src/main.rs
+++ kin/src/main.rs
@@ -1,41 +1,17 @@
-use structopt::StructOpt;
-use quicli::prelude::*;
+mod cmdline;
 
-#[derive(StructOpt)]
-#[structopt(name = "kin", about = "Easy, secure backups for your next of kin")]
-enum CommandLineArgs {
+fn main() -> cmdline::CliResult {
 
-    /// Start a backup project
-    #[structopt(name = "init")]
-    Init {
-
-        /// The directory where you will assemble the things you need to back up
-        #[structopt(parse(from_os_str))]
-        directory: Option<std::path::PathBuf>
-    },
-
-    /// Compile a backup
-    #[structopt(name = "compile")]
-    Compile {
-
-        /// The destination directory where you want to generate the backup
-        #[structopt(name = "destination", parse(from_os_str))]
-        dest_dir: std::path::PathBuf
-    }
-}
-
-fn main() -> CliResult {
-
-    let args = CommandLineArgs::from_args();
+    let args = cmdline::parse();
     let message = match args {
-        CommandLineArgs::Init { directory } => {
+        cmdline::CliArgs::Init { directory } => {
             match directory {
                 Some(dir) => String::from(dir.to_str().unwrap()),
                 None => String::from("No directory specified.")
             }
         },
 
-        CommandLineArgs::Compile { dest_dir } => String::from(dest_dir.to_str().unwrap())
+        cmdline::CliArgs::Compile { dest_dir } => String::from(dest_dir.to_str().unwrap())
     };
 
     println!("{}", message);
