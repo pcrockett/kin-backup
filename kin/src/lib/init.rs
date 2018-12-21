2465e8f54e195b72f5cb828ef9230021b618058c
Philip Crockett <contact@philcrockett.com>
2018-12-21 14:21:43 +0100

    Setting up project dir

--- kin/src/lib/init.rs
+++ kin/src/lib/init.rs
@@ -1,10 +1,35 @@
 use super::cmdline;
+use failure::{ bail };
 use log::{ info };
 use std::fs;
 use std::io;
 use std::path::Path;
+use std::path::PathBuf;
 
 pub fn run(args: cmdline::InitArgs) -> Result<(), failure::Error> {
+
+    let project_dir = get_project_dir(args)?;
+    let is_not_empty = fs::read_dir(&project_dir)?.any(|_| true);
+
+    if is_not_empty {
+        bail!("{} is not empty.", project_dir.to_str().unwrap());
+    }
+
+    let subdirs = [
+            "public",
+            "secret",
+            ".kin"
+        ].iter()
+        .map(|x| project_dir.join(x));
+
+    for subdir in subdirs {
+        ensure_dir(&subdir)?;
+    }
+
+    Ok(())
+}
+
+fn get_project_dir(args: cmdline::InitArgs) -> Result<PathBuf, failure::Error> {
 
     let project_dir = match args.directory {
         Some(dir) => {
@@ -20,9 +45,7 @@ pub fn run(args: cmdline::InitArgs) -> Result<(), failure::Error> {
         None => std::env::current_dir()
     }?;
 
-    info!("{}", project_dir.to_str().unwrap());
-
-    Ok(())
+    Ok(project_dir)
 }
 
 fn ensure_dir(path: &Path) -> io::Result<()> {
