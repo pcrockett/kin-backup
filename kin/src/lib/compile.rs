3e47266fda07901f4e4e080287e288da862a6979
Philip Crockett <contact@philcrockett.com>
2019-01-03 03:07:38 +0100

    Copying public subdirectories

--- kin/src/lib/compile.rs
+++ kin/src/lib/compile.rs
@@ -1,33 +1,49 @@
 use super::cmdline::CompileArgs;
 use super::fsutil;
 use super::kinproject::KinProject;
-use std::path::Path;
+use log::{ info };
+use std::fs;
+use std::path::PathBuf;
 
 pub fn run(args: CompileArgs) -> Result<(), failure::Error> {
 
     fsutil::ensure_empty_dir(&args.dest_dir)?;
     let project = KinProject::from(args.project_dir);
 
-    copy_public_dir(&project, &args.dest_dir)?;
+    copy_public_dir(&project, args.dest_dir)?;
 
     Ok(())
 }
 
-fn copy_public_dir(project: &KinProject, dest_dir: &Path) -> Result<(), failure::Error> {
+fn copy_public_dir(project: &KinProject, dest_dir: PathBuf) -> Result<(), failure::Error> {
 
     let dest_public_dir = dest_dir.join("public");
     fsutil::ensure_empty_dir(&dest_public_dir)?;
 
-    let public_contents = std::fs::read_dir(project.public_dir())?;
+    copy_dir(project.public_dir(), dest_public_dir)?;
 
-    for item in public_contents {
+    Ok(())
+}
+
+fn copy_dir(source: PathBuf, dest: PathBuf) -> Result<(), failure::Error> {
+
+    let contents = fs::read_dir(source)?;
+
+    for item in contents {
         let item = item?;
 
         if item.metadata()?.is_dir() {
-            // TODO: Copy this subdir
+            let new_dest = dest.join(item.file_name());
+            fs::create_dir(&new_dest)?;
+            copy_dir(item.path(), new_dest)?;
         } else {
-            let dest_path = dest_public_dir.join(item.file_name());
-            std::fs::copy(item.path(), dest_path)?;
+            let dest_path = dest.join(item.file_name());
+
+            info!("copying {} to {}...",
+                item.path().to_str().unwrap(),
+                dest_path.to_str().unwrap());
+
+            fs::copy(item.path(), dest_path)?;
         }
     }
 
