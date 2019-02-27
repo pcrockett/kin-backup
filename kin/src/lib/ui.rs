ad9813c7391e5b483e55974870e286b0e12f3854
Philip Crockett <contact@philcrockett.com>
2019-02-27 22:04:41 +0100

    Properly stripping newlines off user input. Closes #19

--- kin/src/lib/ui.rs
+++ kin/src/lib/ui.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::Write;
+use std::iter::Iterator;
 
 pub fn prompt(question: &str) -> Result<String, failure::Error> {
 
@@ -8,7 +9,11 @@ pub fn prompt(question: &str) -> Result<String, failure::Error> {
 
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
-    input.pop().unwrap(); // Remove newline at end
 
-    Ok(input)
+    // input has a newline at the end. On Linux or MacOS that could be an \n.
+    // On Windows it could be an \r\n. `input.lines()` is smart enough to
+    // take care of that for us.
+    let input = input.lines().nth(0).unwrap();
+
+    Ok(String::from(input))
 }
